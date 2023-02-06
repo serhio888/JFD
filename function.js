1cfe40c98d279bbbbd7b6f216cb08ab88d9b237a
serhio <greatganymede@yandex.ru>
2023-02-06 20:54:04 +0500

    function#2

--- function.js
+++ function.js
@@ -12,3 +12,28 @@ const getName3 = name => `you name ${name}`
 console.log(getName1('serg'))
 console.log(getName2('serg'))
 console.log(getName3('serg'))
+
+//#2
+function getSumOfNumbers(number, type="odd"){
+    let sum = 0;
+    if(typeof number != "number") return NaN
+    if(type === "even"){
+        for(let i = 0; i<=number; i++){
+            i%2 === 0 ? sum+=i : null
+        }
+    }
+    if(type === ''){
+        for(let i=0;i<=number;i++){
+            sum +=i
+        }
+    }
+    if(type === "odd"){
+        for(let i = 0; i<=number; i++){
+            i%2 != 0 ? sum+=i : null
+        }
+    }
+
+    return sum
+}
+
+
