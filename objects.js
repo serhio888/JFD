46a0ca33e999a1b34222dec7ec34f451ccb13596
serhio <greatganymede@yandex.ru>
2023-02-10 08:46:26 +0500

    task#2

--- objects.js
+++ objects.js
@@ -19,3 +19,21 @@ const users = [
 
 const onlineUsers = users.filter(elem=>elem.status === 'online')
 console.log(`Сейчас в онлайне пользователи: ${onlineUsers.map(elem=>elem.username)}`)
+
+//#2
+const ordersArr = [4, 2, 1, 3];
+const people = [
+   { id: 1, name: "Максим" },
+   { id: 2, name: "Николай" },
+   { id: 3, name: "Ангелина" },
+   { id: 4, name: "Виталий" },
+];
+function giveTalonsInOrder(pArr,oArr){
+	let arr = []
+	for(id of oArr){
+		arr.push(pArr.find(elem => elem.id === id))
+	}
+	return arr
+} 
+const result = giveTalonsInOrder(people, ordersArr);
+console.log('result', result);
