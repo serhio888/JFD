eebe651be65dbe627525e728b981a5ac1ae365fb
serhio <greatganymede@yandex.ru>
2023-02-17 21:30:23 +0500

    compareAge method #2

--- class.js
+++ class.js
@@ -37,3 +37,26 @@ student.setTechnologies([ 'HTML', 'CSS', 'JavaScript' ,'React']);
 student.setNewStatus('Junior+');
 console.log(student);
 
+class Person {
+    constructor(name,age){
+        this.name = name,
+        this.age = age
+    }
+
+    compareAge(person){
+       if(this.age >= person.age){
+        console.log(this.name + ' старше чем ' + person.name)
+       }else{
+        console.log(this.name + ' младше чем ' + person.name)
+       }
+       
+    }
+}
+
+const person1 = new Person('Максим', 24);
+const person2 = new Person('Светлана', 36);
+const person3 = new Person('Ирина', 23);
+
+person1.compareAge(person2); 
+person2.compareAge(person3); 
+person3.compareAge(person1);
