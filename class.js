6d6995ee377ab580159aeb5ef91f536d1cbf2c02
serhio <greatganymede@yandex.ru>
2023-02-17 22:44:42 +0500

    dictionary #3

--- class.js
+++ class.js
@@ -35,7 +35,7 @@ class Student {
 const student = new Student ('Serg', 34);
 student.setTechnologies([ 'HTML', 'CSS', 'JavaScript' ,'React']);
 student.setNewStatus('Junior+');
-console.log(student);
+//console.log(student);
 
 class Person {
     constructor(name,age){
@@ -57,6 +57,43 @@ const person1 = new Person('Максим', 24);
 const person2 = new Person('Светлана', 36);
 const person3 = new Person('Ирина', 23);
 
-person1.compareAge(person2); 
-person2.compareAge(person3); 
-person3.compareAge(person1);
+// person1.compareAge(person2); 
+// person2.compareAge(person3); 
+// person3.compareAge(person1);
+
+//#3
+
+class Dictionary {
+    constructor (name){
+        this.name = name,
+        this.words = {}
+    }
+
+    add(word,description){
+        if(!(word in this.words)){
+            this.words[word] = {           
+                word,description                    
+            }
+        }            
+    }
+    remove(word){
+        delete this.words[word]
+    }
+    get(word){
+        return this.words[word]
+    }
+    showAllWords(){
+        const wd = Object.entries(this.words).map((el)=>{
+            return el[1]['word'] + '-' + el[1]['description']
+        })
+       return console.log(wd.join(', '))
+    }
+}
+
+const dictionary = new Dictionary('Языки программирования')
+dictionary.add('js','Самый лучший язык программирования')
+dictionary.add('GO','язык программирования разработанный в Google')
+dictionary.add('GO','язык разработанный в Google')
+//console.log(dictionary)
+dictionary.remove('GOO')
+console.log(dictionary.showAllWords())
