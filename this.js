987f44a99c71d873d8cac4ee95907b0524c81581
serhio <greatganymede@yandex.ru>
2023-02-21 12:32:38 +0500

    new object

--- this.js
+++ this.js
@@ -1,61 +1,63 @@
-// //#1
-// const student = {
-//     stack:['HTML'],
-//     level:1,
-//     improveLevel: function(){
-//         this.level++
-//         if(this.level === 2){
-//             this.stack.push('CSS')
-//             return this
-//         }
-//         if(this.level === 3){
-//             this.stack.push('JavaScript')
-//             return this
-//         }
-//         if(this.level === 4){
-//             this.stack.push('React')
-//             return this
-//         }
-//         if(this.level === 5){
-//             this.stack.push('NodeJS')
-//             return this
+//#1
+const student = {
+    stack:['HTML'],
+    level:1,
+    improveLevel: function(){
+        this.level++
+        if(this.level === 2){
+            this.stack.push('CSS')
+            return this
+        }
+        if(this.level === 3){
+            this.stack.push('JavaScript')
+            return this
+        }
+        if(this.level === 4){
+            this.stack.push('React')
+            return this
+        }
+        if(this.level === 5){
+            this.stack.push('NodeJS')
+            return this
             
-//         }
-//         if(this.level > 5){
-//             console.log('Студент выучил все технологии!')
-//             this.level = 5
-//             return this
-//         }
-//     }
-// }
+        }
+        if(this.level > 5){
+            console.log('Студент выучил все технологии!')
+            this.level = 5
+            return this
+        }
+    }
+}
 
-// console.log(student.improveLevel().improveLevel().improveLevel().improveLevel().improveLevel())
+console.log(student.improveLevel().improveLevel().improveLevel().improveLevel().improveLevel())
 
-// //#2
-// const dog = {
-//     name: 'Чарли',
-//     type: 'Собака',
-//     makeSound() {
-//        return 'Гав-Гав';
-//     }
-//  }
+//#2
+const dog = {
+    name: 'Чарли',
+    type: 'Собака',
+    makeSound() {
+       return 'Гав-Гав';
+    }
+ }
  
-//  const bird = {
-//     name: 'Петя',
-//     type: 'Воробей',
-//     makeSound() { 
-//        return 'Чик-чирик';
-//     }
-//  }
-//  function makeDomestic(isDomestic) {
-//     console.log(this.type + ' по имени ' + this.name + ' говорит ' + this.makeSound())
-//     this.isDomestic = isDomestic
-//     return this
-//  }
-//  const animal = makeDomestic.bind(dog)
-//  console.log(animal(true))
-//  console.log(makeDomestic.call(bird,false))
-//  console.log(makeDomestic.apply(bird,[false]))
+ const bird = {
+    name: 'Петя',
+    type: 'Воробей',
+    makeSound() { 
+       return 'Чик-чирик';
+    }
+ }
+ function makeDomestic(isDomestic) {
+    console.log(this.type + ' по имени ' + this.name + ' говорит ' + this.makeSound())
+    return {
+      ...this,
+      isDomestic
+    }
+ }
+ const animal = makeDomestic.bind(dog)
+ console.log(animal(true))
+ console.log(makeDomestic.call(bird,false))
+ console.log(makeDomestic.apply(bird,[false]))
 
  //#3
  const footballer = {
